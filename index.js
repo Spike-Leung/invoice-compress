b8ceee09fb35ce5db0fdf257030a70f3deecc292
Spike <l-yanlei@hotmail.com>
2022-05-03 14:21:39 +0800

    refactor: make code more readable

--- index.js
+++ index.js
@@ -7,48 +7,23 @@ const http = require("http");
 const https = require("https");
 const { JSDOM } = jsdom;
 const handleBaiwangUrl = require("./adapter/baiwang");
+const COMMON_REQUSET_OPTIONS = {
+  headers: { "Content-Type": "application/pdf" },
+};
 
 readFile("./temp/mail.xml", "utf8").then((data) => {
   const mailPaths = [...data.matchAll(/<path>(.*)<\/path>/g)].map(
     ([_, path]) => path
   );
 
-  const linkSet = new Set();
   const promises = [];
 
-  mailPaths.forEach(async (mailPath) => {
-    promises.push(
-      readFile(mailPath, "utf8").then(async (mail) => {
-        let { html } = await smp(mail);
+  mailPaths.forEach(async (mailPath) =>
+    promises.push(resolveMailPdfLink(mailPath))
+  );
 
-        const dom = new JSDOM(html);
-        const anchors = dom.window.document.querySelectorAll("a");
-
-        anchors.forEach((a) => {
-          if (a.href.trim() !== "") {
-            const url = new URL(a.href);
-            const excludeLinkKeywords = [
-              "ad.",
-              "adv.",
-              "pdf/view",
-              "apissp.fapiao.com/pz",
-            ];
-            const activeLink = excludeLinkKeywords.every(
-              (w) => url.href.indexOf(w) === -1
-            );
-            const hasPathName = url.pathname !== "/";
-
-            if (activeLink && hasPathName) {
-              linkSet.add(a.href);
-            }
-          }
-        });
-      })
-    );
-  });
-
-  Promise.all(promises).then(() => {
-    const links = [...linkSet];
+  Promise.all(promises).then((rawLinks) => {
+    const links = [...new Set(rawLinks.flat())];
 
     links.forEach((l, index) => {
       const isHttp = l.indexOf("https") === -1;
@@ -57,66 +32,100 @@ readFile("./temp/mail.xml", "utf8").then((data) => {
   });
 });
 
-var download = function (url, dest, isHttp) {
-  var file = fs.createWriteStream(dest);
-  if (isHttp) {
-    http
-      .get(
-        url,
-        { headers: { "Content-Type": "application/pdf" } },
-        function (response) {
-          response.pipe(file);
+async function resolveMailPdfLink(mailPath) {
+  return readFile(mailPath, "utf8").then(async (mail) => {
+    let { html } = await smp(mail);
+    let links = [];
+    const dom = new JSDOM(html);
+    const anchors = dom.window.document.querySelectorAll("a");
+
+    anchors.forEach((a) => {
+      if (a.href.trim() !== "") {
+        const url = new URL(a.href);
+        const activeLink = filterExcludeLinks(url.href);
+        const hasPathName = url.pathname !== "/";
+
+        if (activeLink && hasPathName) {
+          links.push(a.href);
         }
-      )
-      .on("error", function (err) {
-        console.log({ error: err });
-      });
-  } else {
-    https
-      .get(
-        url,
-        { headers: { "Content-Type": "application/pdf" } },
-        async function (response) {
-          const { statusCode } = response;
+      }
+    });
+    return links;
+  });
+}
 
-          if (url.indexOf("baiwang.com") !== -1) {
-            const pdfUrl = await handleBaiwangUrl(url);
+/**
+ * @param {string} url
+ * @return {boolean} if url match exclude keyword, return false; Else return true
+ */
+function filterExcludeLinks(url) {
+  return ["ad.", "adv.", "pdf/view", "apissp.fapiao.com/pz"].every(
+    (w) => url.indexOf(w) === -1
+  );
+}
 
-            https
-              .get(
-                pdfUrl,
-                { headers: { "Content-Type": "application/pdf" } },
-                function (response) {
-                  file = fs.createWriteStream(dest);
-                  response.pipe(file);
-                }
-              )
-              .on("error", (error) => {
-                console.log(error);
-              });
-          }
+function download(url, dest, isHttp) {
+  isHttp ? downloadByHttp(url, dest) : downloadByHttps(url, dest);
+}
 
-          if (statusCode === 302) {
-            const { location } = response.headers;
+function downloadByHttp(url, dest) {
+  const file = fs.createWriteStream(dest);
 
-            https
-              .get(
-                location,
-                { headers: { "Content-Type": "application/pdf" } },
-                function (response) {
-                  response.pipe(file);
-                }
-              )
-              .on("error", (error) => {
-                console.log(error);
-              });
-          } else {
-            response.pipe(file);
-          }
-        }
-      )
-      .on("error", function (err) {
-        console.log({ error: err });
-      });
-  }
-};
+  http
+    .get(url, COMMON_REQUSET_OPTIONS, function (response) {
+      response.pipe(file);
+    })
+    .on("error", function (err) {
+      console.log({ error: err });
+    });
+}
+
+function downloadByHttps(url, dest) {
+  const file = fs.createWriteStream(dest);
+
+  https
+    .get(url, COMMON_REQUSET_OPTIONS, async function (response) {
+      const { statusCode } = response;
+
+      if (url.indexOf("baiwang.com") !== -1) {
+        handleHttpsBaiWang(url, dest);
+        return;
+      }
+
+      if (statusCode === 302) {
+        const { location } = response.headers;
+        handleHttpsRedirectUrl(location, dest);
+        return;
+      }
+
+      response.pipe(file);
+    })
+    .on("error", function (err) {
+      console.log({ error: err });
+    });
+}
+
+async function handleHttpsBaiWang(url, dest) {
+  const pdfUrl = await handleBaiwangUrl(url);
+
+  https
+    .get(pdfUrl, COMMON_REQUSET_OPTIONS, function (response) {
+      const file = fs.createWriteStream(dest);
+      response.pipe(file);
+    })
+    .on("error", (error) => {
+      console.log(error);
+    });
+}
+
+function handleHttpsRedirectUrl(url, dest) {
+  const file = fs.createWriteStream(dest);
+
+  https
+    .get(url, COMMON_REQUSET_OPTIONS, function (response) {
+      response.pipe(file);
+    })
+    .on("error", (error) => {
+      console.log(error);
+    });
+}
