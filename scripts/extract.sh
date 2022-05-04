51d276d45cf0f98c7aa84bc33e166a5448f29101
Spike <l-yanlei@hotmail.com>
2022-05-04 13:53:54 +0800

    feat: compress all pdf files after extract

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -7,3 +7,7 @@ echo "\n\nExtract mail with pdf..."
 
 echo "\n\nExtract mail with links..."
 ./scripts/extract-link.sh $1
+
+echo "Compress all pdf invoice"
+date=`date '+%Y%m'`
+zip -v ./pdf/invoce_$date.zip ./pdf/*.pdf
